430bab62499baaa1299ed9f17846c4185d368831
caleb l <caleb@calebs-MacBook-Air.local>
2021-06-16 12:27:32 +0800

    updated feed ui

--- src/Feed/Main.js
+++ src/Feed/Main.js
@@ -1,24 +1,125 @@
-import React, { useState, useEffect, } from 'react';
+import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { View, Text, SafeAreaView } from 'react-native'
-import { firebase } from '../shared/config'
-import { globalStyles } from '../shared/globalStyles'
-import HawkerfeedScreen from "./HawkerFeed"
-import RecipefeedScreen from "./RecipeFeed"
-import HawkerCard from "../HawkerCard"
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
+import { firebase } from "../shared/config";
+import { globalStyles } from "../shared/globalStyles";
+import HawkerfeedScreen from "./HawkerFeed";
+import RecipefeedScreen from "./RecipeFeed";
+import HawkerCard from "../HawkerCard";
+import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
 
 export default function Home({ navigation }) {
-    const [feed, setFeed] = useState("recipe")
-    const [hidden, setHidden] = useState(false)
+  const [feed, setFeed] = useState("recipe");
+  const [hidden, setHidden] = useState(false);
+  const [modal, setModalVisible] = useState(false);
+  const [modalRecipe, setModalRecipeVisible] = useState(false);
 
-    return (
-        <View style={globalStyles.container}>
-            {!hidden && <><Text style={{ left: 130, top: 50, zIndex: 100, backgroundColor: "transparent", fontSize: 20, position: "absolute", color: feed === "recipe" ? "blue" : 'white', fontWeight: feed === "recipe" ? "bold" : "normal" }} onPress={() => setFeed("recipe")}>Recipe</Text>
-                <Text style={{ left: 200, top: 50, zIndex: 100, backgroundColor: "transparent", fontSize: 20, position: "absolute", color: feed === "hawker" ? "blue" : 'white', fontWeight: feed === "hawker" ? "bold" : "normal" }} onPress={() => setFeed("hawker")}>Hawker</Text></>}
-            <>
-                {feed === "recipe" ? <RecipefeedScreen setHidden={setHidden} /> : <HawkerfeedScreen setHidden={setHidden} />}
-            </>
-        </View >
-    )
+  return (
+    <View style={globalStyles.container}>
+      {!hidden && (
+        <>
+          <FontAwesome
+            name="filter"
+            size={30}
+            style={{
+              left: 25,
+              top: 55,
+              zIndex: 100,
+              backgroundColor: "transparent",
+              position: "absolute",
+              color: "white",
+              opacity: 0.8,
+            }}
+            onPress={() => setModalVisible(true)}
+          />
+          <Modal
+            animationType="slide"
+            visible={modal}
+            onRequestClose={() => {
+              setModalVisible(false);
+            }}
+          >
+            <TouchableOpacity
+              style={{ marginTop: 40, marginLeft: 30 }}
+              onPress={() => {
+                setModalVisible(false);
+              }}
+            >
+              <MaterialCommunityIcons
+                name="close-circle"
+                size={32}
+                color="black"
+              />
+            </TouchableOpacity>
+            <View style={globalStyles.container}>
+              <Text>filters here</Text>
+            </View>
+          </Modal>
+          <Text
+            style={{
+              fontFamily:
+                feed === "recipe" ? "quicksand-bold" : "quicksand-regular",
+              left: 135,
+              top: 60,
+              zIndex: 100,
+              backgroundColor: "transparent",
+              fontSize: 20,
+              position: "absolute",
+              color: feed === "recipe" ? "#648cd4" : "white",
+              opacity: feed === "recipe" ? 1.0 : 0.8,
+            }}
+            onPress={() => setFeed("recipe")}
+          >
+            Recipe
+          </Text>
+          <Text
+            style={{
+              fontFamily: "quicksand-bold",
+              left: 205,
+              top: 60,
+              zIndex: 100,
+              backgroundColor: "transparent",
+              fontSize: 20,
+              position: "absolute",
+              color: "white",
+              opacity: 0.5,
+            }}
+          >
+            |
+          </Text>
+          <Text
+            style={{
+              fontFamily:
+                feed === "hawker" ? "quicksand-bold" : "quicksand-regular",
+              left: 215,
+              top: 60,
+              zIndex: 100,
+              backgroundColor: "transparent",
+              fontSize: 20,
+              position: "absolute",
+              color: feed === "hawker" ? "#648cd4" : "white",
+              opacity: feed === "hawker" ? 1.0 : 0.8,
+            }}
+            onPress={() => setFeed("hawker")}
+          >
+            Hawker
+          </Text>
+        </>
+      )}
+      <>
+        {feed === "recipe" ? (
+          <RecipefeedScreen setHidden={setHidden} />
+        ) : (
+          <HawkerfeedScreen setHidden={setHidden} />
+        )}
+      </>
+    </View>
+  );
 }
--- src/shared/globalStyles.js
+++ src/shared/globalStyles.js
@@ -1,155 +1,164 @@
 import { StyleSheet } from "react-native";
 
 export const colorTheme = {
-    //populate this with common colour themes we are using
+  //populate this with common colour themes we are using
 
-    backgroundWhite: "white",
-    backgroundBlueOne: "#acbcec",
-    backgroundBlueTwo: "#8cbcdc",
-    backgroundBlueThree: "#abdbe3",
-    backgroundBlueFour: "#648cd4",
-    TextBlue: "#4c74cc",
-    TextBlack: "black",
+  backgroundWhite: "white",
+  backgroundBlueOne: "#acbcec",
+  backgroundBlueTwo: "#8cbcdc",
+  backgroundBlueThree: "#abdbe3",
+  backgroundBlueFour: "#648cd4",
+  TextBlue: "#4c74cc",
+  TextBlack: "black",
 };
 
 export const tabsNavigatorStyle = {
-    color: {
-        feed: colorTheme.backgroundBlueOne,
-        createPost: colorTheme.backgroundBlueTwo,
-        profilePage: colorTheme.backgroundBlueThree,
-    },
-    font: {
-        fontFamily: "quicksand-regular",
-    },
+  color: {
+    feed: colorTheme.backgroundBlueOne,
+    createPost: colorTheme.backgroundBlueTwo,
+    profilePage: colorTheme.backgroundBlueThree,
+  },
+  font: {
+    fontFamily: "quicksand-regular",
+  },
 };
 
 export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colorTheme.backgroundWhite,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    container: {
-        flex: 1,
-        backgroundColor: colorTheme.backgroundWhite,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  titleText: {
+    fontFamily: "quicksand-bold",
+    fontSize: 40,
+    paddingHorizontal: 7,
+    paddingVertical: 2,
+    marginVertical: 10,
+    textAlign: "center",
+  },
 
-    titleText: {
-        fontFamily: 'quicksand-bold',
-        fontSize: 40,
-        paddingHorizontal: 7,
-        paddingVertical: 2,
-        marginVertical: 10,
-        textAlign: 'center'
-    },
+  secondaryTitleText: {
+    fontSize: 18,
+    fontFamily: "quicksand-bold",
+    textAlign: "right",
+  },
 
-    secondaryTitleText: {
-        fontSize: 18,
-        fontFamily: 'quicksand-bold',
-        textAlign: 'right',
-    },
-
-    otherText: {
-        justifyContent: 'flex-start',
-        left: 0,
-        fontSize: 18,
-        fontFamily: 'quicksand-bold',
-    },
-    input: {
-        height: 60,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: '#f2f2f2',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16,
-        fontFamily: 'quicksand-regular',
-    },
-    button: {
-        backgroundColor: colorTheme.backgroundBlueTwo,
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 60,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center',
-        fontFamily: 'quicksand-regular',
-    },
-    footerView: {
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: '#2e2e2d',
-        fontFamily: 'quicksand-regular',
-    },
-    footerLink: {
-        color: "#134F5C",
-        fontSize: 16,
-        fontFamily: 'quicksand-bold',
-    },
-    profileName: {
-        color: "#134F5C",
-        fontSize: 20,
-        fontFamily: 'quicksand-bold',
-        textAlign: "center",
-    },
-    profilePicture: {
-        width: 50,
-        height: 50,
-        borderRadius: 100,
-    },
-    ProfileboxWrapper: {
-        borderBottomColor: '#dddddd',
-        borderBottomWidth: 1,
-        borderTopColor: '#dddddd',
-        borderTopWidth: 1,
-        flexDirection: 'row',
-        height: 100,
-    },
-    Profilebox: {
-        width: '33%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRightColor: '#dddddd',
-        borderRightWidth: 1,
-    },
-    logOut: {
-        padding:10,
-        borderRadius: 10,
-        width: "25%",
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: colorTheme.backgroundBlueThree
-    },
-    createPostView: {
-        margin: 10,
-    },
-    createPostText: {
-        justifyContent: "flex-start",
-        left: 10,
-        fontSize: 18,
-        fontFamily: "quicksand-bold",
-    },
-    createPostInput: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingLeft: 10,
-    },
-    createPostInputLong: {
-        height: 140,
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingLeft: 10,
-    },
-    createPostButton: {
-        alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 20,
-    },
+  otherText: {
+    justifyContent: "flex-start",
+    left: 0,
+    fontSize: 18,
+    fontFamily: "quicksand-bold",
+  },
+  input: {
+    height: 60,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "#f2f2f2",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+    fontFamily: "quicksand-regular",
+  },
+  button: {
+    backgroundColor: colorTheme.backgroundBlueTwo,
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 60,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "quicksand-regular",
+  },
+  footerView: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+    fontFamily: "quicksand-regular",
+  },
+  footerLink: {
+    color: "#134F5C",
+    fontSize: 16,
+    fontFamily: "quicksand-bold",
+  },
+  profileName: {
+    color: "#134F5C",
+    fontSize: 20,
+    fontFamily: "quicksand-bold",
+    textAlign: "center",
+  },
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+  ProfileboxWrapper: {
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+    borderTopColor: "#dddddd",
+    borderTopWidth: 1,
+    flexDirection: "row",
+    height: 100,
+  },
+  Profilebox: {
+    width: "33%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRightColor: "#dddddd",
+    borderRightWidth: 1,
+  },
+  logOut: {
+    padding: 10,
+    borderRadius: 10,
+    width: "25%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colorTheme.backgroundBlueThree,
+  },
+  createPostView: {
+    margin: 10,
+  },
+  createPostText: {
+    justifyContent: "flex-start",
+    left: 10,
+    fontSize: 18,
+    fontFamily: "quicksand-bold",
+  },
+  createPostInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  createPostInputLong: {
+    height: 140,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  createPostButton: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 20,
+  },
+  feedTitleText: {
+    fontFamily: "quicksand-bold",
+    fontSize: 40,
+    textAlign: "left",
+  },
+  feedSubtitleText: {
+    fontFamily: "quicksand-regular",
+    fontSize: 18,
+    textAlign: "left",
+  },
 });
